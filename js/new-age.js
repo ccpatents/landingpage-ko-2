4ed1bcadf82d9c630f454c19e646be4fc3e5c26b
ccpatents <ccpatents@github.com>
2020-03-15 12:53:06 +0900

    .

--- js/new-age.js
+++ js/new-age.js
@@ -48,6 +48,8 @@ var video6;
   // Collapse the navbar when page is scrolled
   $(window).scroll(navbarCollapse);
 
+ 
+
   setTimeout(function () {
     var tag = document.createElement('script');
 
@@ -56,6 +58,18 @@ var video6;
     firstScriptTag
       .parentNode
       .insertBefore(tag, firstScriptTag);
+
+      var siteStellar = function() {
+        $(window).stellar({
+          responsive: false,
+          parallaxBackgrounds: true,
+          parallaxElements: true,
+          horizontalScrolling: false,
+          hideDistantElements: false,
+          scrollProperty: 'scroll'
+        });
+      };
+      siteStellar();
   }, 300);
 
   $('.close-modal').click(function () {
